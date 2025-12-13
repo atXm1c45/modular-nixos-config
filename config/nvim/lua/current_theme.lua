04ef00981ec221626c43165b4d90c43bb3bee29d
atXm1c45 <neilmatthewfprudencio@gmail.com>
2025-12-13 12:12:05 +0800

    Some configuration stuff, linked neovim to current noctalia theme

--- config/nvim/lua/current_theme.lua
+++ config/nvim/lua/current_theme.lua
@@ -1,16 +1,16 @@
 -- Matugen-generated theme for Neovim
 return {
-    background = "#0f1416",
-    foreground = "#dee3e6",
-    cursor     = "#87d1eb",
+    background = "#131313",
+    foreground = "#e2e2e2",
+    cursor     = "#71dba6",
 
     -- Core UI Colors
-    normal     = "#dee3e6",
-    comment    = "#8a9296",
-    constant   = "#c2c3eb",
-    string     = "#b3cad4",
-    function_  = "#87d1eb",
-    keyword    = "#c2c3eb",
+    normal     = "#e2e2e2",
+    comment    = "#919191",
+    constant   = "#a4cddd",
+    string     = "#b4ccbc",
+    function_  = "#71dba6",
+    keyword    = "#a4cddd",
     error      = "#ffb4ab",
     warning    = "#ffb4ab", -- Fallback to error color if warning isn't separate
 }
