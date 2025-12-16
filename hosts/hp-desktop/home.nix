7f8f8e2cedbbed14ed0262cdcc11ecec36544d11
atXm1c45 <92849575+atXm1c45@users.noreply.github.com>
2025-12-16 13:18:19 +0800

    Changed fastfetch into a flake-part

--- hosts/hp-desktop/home.nix
+++ hosts/hp-desktop/home.nix
@@ -18,10 +18,6 @@
   programs.home-manager.enable = true;
   programs.git.enable = true;
 
-  home.packages = with pkgs; [
-    fastfetch
-  ];
-
   xdg.desktopEntries = {
     thunar-bulk-rename = {
       name = "thunar-bulk-rename";
@@ -39,9 +35,5 @@
       name = "vim";
       noDisplay = true;
     };
-  };
-
-  xdg.configFile."config.jsonc" = {
-    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-dotfiles/config/fastfetch/";
   };
 }
