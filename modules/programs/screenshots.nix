4be596205d17450021f3b68907488ec129d4e125
atXm1c45 <92849575+atXm1c45@users.noreply.github.com>
2025-12-16 13:51:09 +0800

    Accidentally removed my timestamps (yikes), anyways changed screenshotting util

--- modules/programs/screenshots.nix
+++ modules/programs/screenshots.nix
@@ -1,23 +1,8 @@
 { inputs, config, pkgs, ... }:
 
-let
-  screenshotScript = pkgs.writeShellScriptBin "screenshot" ''
-    # 1. Generate filename with date
-    DATE=$(date +%Y%m%d_%H%M%S)
-    FILE_NAME="$HOME/Pictures/Screenshot_''${DATE}.png"
-
-    # 2. Use Nix-managed paths for tools so they NEVER fail
-    ${pkgs.grim}/bin/grim -g "$(${pkgs.slurp}/bin/slurp)" "$FILE_NAME"
-
-    # 3. Copy to clipboard
-    ${pkgs.wl-clipboard}/bin/wl-copy < "$FILE_NAME"
-  '';
-in
 {
   home.packages = with pkgs; [
-    screenshotScript
-    grim
-    slurp
     wl-clipboard
+    satty
   ];
 }
