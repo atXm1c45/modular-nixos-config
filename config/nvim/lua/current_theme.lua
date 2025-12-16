4e6cdf9d8ca7dde176c55835f74299167bfd15e5
atXm1c45 <92849575+atXm1c45@users.noreply.github.com>
2025-12-16 13:18:19 +0800

    Some personal configuration added, Oh-my-posh added

---
+++ config/nvim/lua/current_theme.lua
@@ -0,0 +1,16 @@
+-- Matugen-generated theme for Neovim
+return {
+    background = "#0f1416",
+    foreground = "#dee3e6",
+    cursor     = "#87d1eb",
+
+    -- Core UI Colors
+    normal     = "#dee3e6",
+    comment    = "#8a9296",
+    constant   = "#c2c3eb",
+    string     = "#b3cad4",
+    function_  = "#87d1eb",
+    keyword    = "#c2c3eb",
+    error      = "#ffb4ab",
+    warning    = "#ffb4ab", -- Fallback to error color if warning isn't separate
+}
--- modules/programs/zsh.nix
+++ modules/programs/zsh.nix
@@ -20,17 +20,20 @@
       ];
     };
 
-    initContent = "source ${config.home.homeDirectory}/nixos-dotfiles/config/zsh/aliases.zsh";
+    initExtra = ''
+      source "${config.home.homeDirectory}/nixos-dotfiles/config/zsh/aliases.zsh"
 
-    oh-my-zsh = {
-      enable = true;
-      plugins = ["git" "sudo"];
-    };
+      eval "$(oh-my-posh init zsh --config ${config.xdg.configHome}/oh-my-posh/theme.json)"
+    '';
   };
+
+  xdg.configFile."oh-my-posh/theme.json".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-dotfiles/config/zsh/theme.json";
 
   home.sessionPath = [
     "${pkgs.stylua}/bin"
     "${pkgs.ruff}/bin"
-    # Add any other formatter that was giving Exit 127 here
   ];
+
+  home.packages = [pkgs.oh-my-posh];
 }
